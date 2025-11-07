8387ad15bdfd059c200bbf12970658f92618ec49
supporspanish-lab <supporspanish@gmail.com>
2025-11-06 22:13:07 -0300

    Delete pool directory

--- pool/login/home-ecensiales/firebaseService.js
+++
@@ -1,4 +0,0 @@
-import { db, auth, getDoc } from '../auth.js';
-import { collection, query, where, getDocs, addDoc, doc, updateDoc, onSnapshot, deleteDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
-
-export { db, auth, getDoc, collection, query, where, getDocs, addDoc, doc, updateDoc, onSnapshot, deleteDoc, arrayUnion };
